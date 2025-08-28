bd2caf8ae59693df0b22345d08635523d103984a
marsil1990 <ma.silveram@gmail.com>
2025-08-28 19:00:19 -0300

    week1

---
+++ scripts/dates.js
@@ -0,0 +1,5 @@
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = "Last Modified " + lastModified;
+const yearSpan = document.getElementById("currentyear");
+const currentYear = new Date().getFullYear();
+yearSpan.textContent = currentYear;
